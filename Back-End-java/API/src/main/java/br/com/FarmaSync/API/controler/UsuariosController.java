7bf64e9c0bc38f69797606f67fe6048857f6d0e1
Davi-dev1 <davigancalveesdasilva@gmail.com>
2026-09-20 21:56:30 -0300

    Teste

--- Back-End-java/API/src/main/java/br/com/FarmaSync/API/controler/UsuariosController.java
+++ Back-End-java/API/src/main/java/br/com/FarmaSync/API/controler/UsuariosController.java
@@ -1,4 +1,46 @@
 package br.com.FarmaSync.API.controler;
 
+import br.com.FarmaSync.API.models.Usuarios;
+import br.com.FarmaSync.API.repository.UsuariosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/usuarios")
 public class UsuariosController {
+
+
+
+    @Autowired
+    private UsuariosRepository usuariosRepository;
+//TODO: Aqui coloque o @Autowired do Password encoder
+
+
+    @GetMapping("/")
+    public List<Usuarios> findAll(){
+        return usuariosRepository.findAll();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- Back-End-java/API/src/main/java/br/com/FarmaSync/API/repository/UsuariosRepository.java
+++ Back-End-java/API/src/main/java/br/com/FarmaSync/API/repository/UsuariosRepository.java
@@ -1,4 +1,7 @@
 package br.com.FarmaSync.API.repository;
 
-public interface UsuariosRepository {
+import br.com.FarmaSync.API.models.Usuarios;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuariosRepository extends JpaRepository<Usuarios,Long> {
 }
